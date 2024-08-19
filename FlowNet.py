1e630a1253b32ba96f20d9e0c7ae6c622dd42bfa
young0428 <dudgb2380@naver.com>
2024-08-19 19:40:14 +0900

    optic_flow_esitimation

---
+++ FlowNet.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm3d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv3d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm3d(out_channels)
+            )
+    
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+
+class Encoder3D(nn.Module):
+    def __init__(self, block, layer_configs):
+        super(Encoder3D, self).__init__()
+        self.in_channels = layer_configs[0][0]  # 첫 번째 레이어의 채널 수로 초기화
+        
+        self.conv1 = nn.Conv3d(4, self.in_channels, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm3d(self.in_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+        
+        self.layers = self._make_layers(block, layer_configs)
+    
+    def _make_layers(self, block, layer_configs):
+        layers = []
+        for out_channels, blocks in layer_configs:
+            stride = 2 if self.in_channels != out_channels else 1
+            layers.append(self._make_layer(block, out_channels, blocks, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+    
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        outputs = []
+        for layer in self.layers:
+            x = layer(x)
+            outputs.append(x)
+        
+        return outputs
+
+class Decoder3D(nn.Module):
+    def __init__(self, layer_configs, num_classes=2):
+        super(Decoder3D, self).__init__()
+        self.upconvs = nn.ModuleList()
+        self.convs = nn.ModuleList()
+        
+        for i in range(len(layer_configs)-1, 0, -1):
+            in_channels = layer_configs[i][0]
+            out_channels = layer_configs[i-1][0]
+            self.upconvs.append(nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2))
+            self.convs.append(nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1))
+        
+        self.final_conv = nn.Conv3d(layer_configs[0][0], num_classes, kernel_size=1)
+    
+    def forward(self, encoder_outputs):
+        x = encoder_outputs[-1]
+        for i in range(len(self.upconvs)):
+            x = self.upconvs[i](x)
+            x = torch.cat([x, encoder_outputs[-(i+2)]], dim=1)
+            x = F.relu(self.convs[i](x))
+        
+        x = self.final_conv(x)
+        return x
+
+class FlowNet3D(nn.Module):
+    def __init__(self, block, layer_configs, num_classes=2):
+        super(FlowNet3D, self).__init__()
+        self.encoder = Encoder3D(block, layer_configs)
+        self.decoder = Decoder3D(layer_configs, num_classes)
+    
+    def forward(self, x):
+        encoder_outputs = self.encoder(x)
+        out = self.decoder(encoder_outputs)
+        return out
+
+def flownet3d(layer_configs, num_classes = 2):
+    return FlowNet3D(ResidualBlock, layer_configs, num_classes)
+
+# 예시: 다양한 형태의 layer_configs를 입력으로 모델을 구성
+layer_configs = [[64, 2], [128, 2], [256, 2], [512, 2]]
+model = flownet3d(layer_configs)
+
+# 테스트용 입력 데이터 생성
+x = torch.randn((1, 4, 32, 128, 128))  # Batch size: 1, Channels: 4, Depth: 32, Height: 128, Width: 128
+output = model(x)
+
+print(output.shape)  # 출력 크기 확인
