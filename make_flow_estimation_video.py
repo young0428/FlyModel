a9f97cd758b2adb6f48a97673026d6792450e924
young0428 <dudgb2380@naver.com>
2024-08-20 19:13:38 +0900

    make video done

--- make_flow_estimation_video.py
+++ make_flow_estimation_video.py
@@ -42,7 +42,8 @@ fold_factor = 8
 layer_configs = [[64, 2], [128, 2], [256, 2], [512, 2]]
 
 video_data, total_frame = load_video_data(folder_path, downsampling_factor)
-
+video_data = aug_videos(video_data)
+print(f"augmented shape : {video_data.shape}")
 
 # Split period and split for training / test data set
 recent_losses = deque(maxlen=100)
@@ -56,25 +57,12 @@ model = flownet3d(layer_configs, num_classes=2)
 trainer = Trainer(model, loss_function, lr)
 #%%
 test_tuples = []
-#for i in range(3):
-test_tuples.extend([(0, frame_num) for frame_num in range(frame_per_window, total_frame, 1)])
+for i in range(3):
+    test_tuples.extend([(i, frame_num) for frame_num in range(frame_per_window, total_frame, 1)])
 #%%
-checkpoint_name = '64x128_opticflow_64t51216frames'
-fold = 0
-#for fold in range(5):
-fold_path = f"./best_model/{checkpoint_name}/fold{fold+1}"
-_ = trainer.load(f"{fold_path}/best_model.ckpt")
-video = []
-#%%
-for idx, t in enumerate(test_tuples):
-    print(f"{idx} / {len(test_tuples)}")
-    batch = [t]
-    input_data, target_data = get_data_from_batch_flow_estimate(video_data, batch, frame_per_window)
-    batch_input_data = torch.tensor(input_data, dtype=torch.float32).to(trainer.device)
-    prediction = trainer.pred(batch_input_data)
-    video.append(prediction[0,0])
-#%%
-    
-    
+checkpoint_name = 'best_model'
+for fold in range(5):
+    fold_path = f"./best_model/fold_{fold+1}"
+    _ = trainer.load(f"{fold_path}/{checkpoint_name}.ckpt")
     
     
