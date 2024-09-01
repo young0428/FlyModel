d2df172cd8bdb934c18e46701667b086d57b95a9
young0428 <dudgb2380@naver.com>
2024-09-01 20:28:23 +0900

    Pretrained encoder 적용 / spatial attention 및 feature extraction 적용

--- FlowNet.py
+++ FlowNet.py
@@ -130,6 +130,89 @@ def loss_function(pred, target):
     loss = F.mse_loss(pred, target)
     return loss
 
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        
+        self.conv = nn.Conv3d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
+
+class FlowNet3DWithFeatureExtraction(nn.Module):
+    def __init__(self, flownet3d, feature_dim=128, input_size=(16, 64, 128)):
+        super(FlowNet3DWithFeatureExtraction, self).__init__()
+        self.flownet3d = flownet3d
+        depth, height, width = input_size
+        
+        # Encoder와 Decoder의 파라미터를 고정 (freeze)
+        for param in self.flownet3d.encoder.parameters():
+            param.requires_grad = False
+        for param in self.flownet3d.decoder.parameters():
+            param.requires_grad = False
+        
+        # Decoder 각 단계에서 나오는 feature를 변환하는 FC layer 정의
+        self.fc_layers = nn.ModuleList()
+        for i in range(len(self.flownet3d.decoder.upconvs)):
+            in_channels = flownet3d.decoder.convs[i].out_channels
+            
+            # 각 레이어에서 Conv와 UpConv가 적용된 후의 출력 크기를 계산
+            depth = self._calculate_conv_output_size(depth, kernel_size=3, padding=1, stride=1)
+            height = self._calculate_conv_output_size(height, kernel_size=3, padding=1, stride=1)
+            width = self._calculate_conv_output_size(width, kernel_size=3, padding=1, stride=1)
+            
+            # 그 결과에 따라 Linear 레이어의 입력 크기를 동적으로 결정
+            linear_input_size = feature_dim * depth * height * width
+            
+            self.fc_layers.append(nn.Sequential(
+                nn.Conv3d(in_channels, feature_dim, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv3d(feature_dim, feature_dim, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Flatten(),
+                nn.Linear(linear_input_size, feature_dim),  # Adjusted dynamically based on input size
+                nn.ReLU(inplace=True)
+            ))
+        
+        # 최종 스칼라 값을 출력하는 FC layer
+        self.final_fc = nn.Linear(feature_dim * len(self.fc_layers), 1)
+
+    def _calculate_conv_output_size(self, size, kernel_size, padding, stride):
+        """Convolution 레이어가 적용된 후의 크기를 계산하는 함수"""
+        return (size - kernel_size + 2 * padding) // stride + 1
+    
+    def forward(self, x):
+        x = self.flownet3d.swap_axis_for_input(x)
+        encoder_outputs = self.flownet3d.encoder(x)
+        
+        features = []
+        decoder_output = encoder_outputs[-1]
+        for i in range(len(self.flownet3d.decoder.upconvs)):
+            decoder_output = self.flownet3d.decoder.upconvs[i](decoder_output)
+            decoder_output = torch.cat([decoder_output, encoder_outputs[-(i+2)]], dim=1)
+            decoder_output = F.relu(self.flownet3d.decoder.convs[i](decoder_output))
+            
+            # 각 단계에서의 feature 추출
+            feature = self.fc_layers[i](decoder_output)
+            features.append(feature)
+        
+        # 모든 feature를 종합하여 최종 스칼라 값 출력
+        combined_features = torch.cat(features, dim=1)
+        scalar_output = self.final_fc(combined_features)
+        
+        return scalar_output
+
+ 
+
+
+
 #예시: 다양한 형태의 layer_configs를 입력으로 모델을 구성
 # layer_configs = [[64, 2], [128, 2], [256, 2], [512, 2]]
 # model = flownet3d(layer_configs, num_classes=2)
