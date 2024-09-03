965e2d295726d30eb4f40efe1ebbf03e8c406d12
young0428 <dudgb2380@naver.com>
2024-09-03 17:33:52 +0900

    add sh

---
+++ docker_pytorch_latest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 사용법: ./run_docker.sh <ct_name>
+
+# 컨테이너 이름을 첫 번째 인자로 받습니다.
+CT_NAME=$1
+
+# 현재 사용자의 홈 디렉토리를 변수에 저장합니다.
+HOME_DIR="/home/$USER"
+
+# 도커 실행 명령어
+sudo docker run -it --name ${CT_NAME} -v ${HOME_DIR}:/myhome --gpus all pytorch/pytorch:latest
+
